6046f1634f2a5addba8842ffd2a6db1e8df279b3
Eduardo G. S. Pereira <eduardopereira@modclima.com.br>
2019-07-09 15:22:03 -0300

    core: remove parse types from initSequelize.js

--- src/lib/database/initSequelize.js
+++ src/lib/database/initSequelize.js
@@ -1,17 +1,7 @@
-const pg = require('pg');
 const Sequelize = require('sequelize');
-const moment = require('moment-timezone');
 const config = require('../../../database/config');
 
 const initSequelize = callback => {
-  const OID_BIGINT = 20;
-  const OID_DATE = 1082;
-  const OID_TIMESTAMP = 1114;
-
-  pg.types.setTypeParser(OID_BIGINT, val => Number.parseInt(val, 10));
-  pg.types.setTypeParser(OID_DATE, val => moment.tz(val, 'YYYY-MM-DD', 'UTC'));
-  pg.types.setTypeParser(OID_TIMESTAMP, val => moment.tz(val, 'YYYY-MM-DD HH:mm:ss.SSS', 'UTC'));
-
   const sequelize = new Sequelize({
     ...config[process.env.NODE_ENV || 'development'],
     define: { underscored: true, freezeTableName: true },
